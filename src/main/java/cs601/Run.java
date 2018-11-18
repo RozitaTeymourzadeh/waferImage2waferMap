74be11f335c144ca8ca970b8aacf7f827d517628
Rozita Teymourzadeh <rteymourzadeh@dons.usfca.edu>
2018-11-18 01:45:04 -0800

    Reading Image files

--- src/main/java/cs601/Run.java
+++ src/main/java/cs601/Run.java
@@ -3,6 +3,8 @@
  */
 package cs601;
 
+import java.io.File;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,8 +14,26 @@ import org.apache.logging.log4j.Logger;
  */
 public class Run {
 	private static Logger LOG = LogManager.getLogger(Run.class);
+	private static File cacheFolder = null;
 	
 	public static void main(String[] args) {
+		
 		LOG.info("Convesion is started ..." + Run.class.getName());
+		ConfigManager.getConfig().getInput();
+		
+		File folder = new File(".\\" + ConfigManager.getConfig().getInput());
+		cacheFolder = new File(folder.getParent(), "cache");
+
+		File[] imgs = folder.listFiles();
+		for(int i = 0; i < imgs.length; i++){
+			if(imgs[i].getName().toLowerCase().endsWith(".jpg") || imgs[i].getName().toLowerCase().endsWith(".bmp")|| imgs[i].getName().toLowerCase().endsWith(".png")){
+				convert(imgs[i]);
+			}
+		}
+	}
+
+	private static void convert(File file) {
+		// TODO Auto-generated method stub
+		
 	}
 }

5a7c642c86ed5baa05971fb223f8c06cbd8edbc2
Rozita Teymourzadeh <rteymourzadeh@dons.usfca.edu>
2018-11-17 21:12:53 -0800

    add log configuration file

---
+++ src/main/java/cs601/Run.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package cs601;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @author rozitateymourzadeh
+ *
+ */
+public class Run {
+	private static Logger LOG = LogManager.getLogger(Run.class);
+	
+	public static void main(String[] args) {
+		LOG.info("Convesion is started ..." + Run.class.getName());
+	}
+}

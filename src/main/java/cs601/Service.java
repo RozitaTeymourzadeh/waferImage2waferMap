83e7d5ac168c4d6042ba3db87fbee3ce349955e5
Rozita Teymourzadeh <rteymourzadeh@dons.usfca.edu>
2018-11-21 10:17:32 -0800

    save cropped image

--- src/main/java/cs601/Service.java
+++ src/main/java/cs601/Service.java
@@ -3,7 +3,13 @@
  */
 package cs601;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
 import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -27,6 +33,37 @@ public class Service {
 		} catch(Exception e) {
 			LOG.error("FATAL: Exception occured while generating cache file in: " + Run.class.getName());
 		}
+	}
+	
+	/**
+	 * Save PNG image
+	 * @param BufferdImage
+	 * @param String 
+	 */
+	public void savePNG(final BufferedImage bi, final String path){
+		try {
+			RenderedImage rendImage = bi;
+			ImageIO.write(rendImage, "png", new File(path));
+		} catch ( IOException e) {
+			LOG.error("FATAL: failed to save PNG file.");
+		}
+	}
+	
+	/**
+	 * Buffer the image and return buffered Image
+	 * @param int X
+	 * @param int Y
+	 *  
+	 * @return Buffered Image
+	 */	
+	public BufferedImage map(int sizeX, int sizeY){
+		final BufferedImage imageResult = new BufferedImage( sizeX, sizeY, BufferedImage.TYPE_INT_RGB );
+		for (int x = 0; x < sizeX; x++){
+			for (int y = 0; y < sizeY; y++){
+				imageResult.setRGB(x, y, Color.WHITE.getRGB() );
+			}
+		}
+		return imageResult;
 	}
 
 }

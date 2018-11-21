96376c1070659f86f6df84b5a5e25e0bb0db8bdb
Rozita Teymourzadeh <rteymourzadeh@dons.usfca.edu>
2018-11-21 15:04:09 -0800

    bug fix

--- src/main/java/cs601/FindPattern.java
+++ src/main/java/cs601/FindPattern.java
@@ -6,16 +6,17 @@ public class FindPattern {
 	
 	private float thrLeft = Float.parseFloat(ConfigManager.getConfig().getThrLeft());
 	private float thrRight = Float.parseFloat(ConfigManager.getConfig().getThrRight());
+	Service srv = new Service();
 	
 	public boolean[] findLeft(BufferedImage img, BufferedImage pattern, int x, int y){
-		boolean[] resultLineLeft = new boolean[100];
+		boolean[] res = new boolean[100];
 		int index = 100 - 1;
 		for( ; index >= 0 && x >= 1; x -= pattern.getWidth(), index--){
 			float[][] sim = new float[3][3];
 
 			for(int i = -1; i <= 1; i++){
 				for(int j = -1; j <= 1; j++){
-					sim[i+1][j+1] = this.getSimilarity(img, x+i, y+j, pattern);
+					sim[i+1][j+1] = getSimilarity(img, x+i, y+j, pattern);
 				}	
 			}
 
@@ -32,30 +33,30 @@ public class FindPattern {
 			}
 
 			if(sim[xTemp][yTemp] > thrLeft){
-				resultLineLeft[index] = true;
+				res[index] = true;
 				y += yTemp - 1;
 				x += xTemp - 1;
 			} else {
-				resultLineLeft[index] = false;
+				res[index] = false;
 			}
 		}
 
 		for(; index >= 0; index--){
-			resultLineLeft[index] = false;
+			res[index] = false;
 		}
 
-		return resultLineLeft;
+		return res;
 	}
 
 	public boolean[] findRight(BufferedImage img, BufferedImage pattern, int x, int y){
-		boolean[] resultLineRight = new boolean[100];
+		boolean[] res = new boolean[100];
 		int index = 0;
 		for( ; index < 100 && x < img.getWidth() - pattern.getWidth() - 1; x += pattern.getWidth(), index++){
 			float[][] sim = new float[3][3];
 
 			for(int i = -1; i <= 1; i++){
 				for(int j = -1; j <= 1; j++){
-					sim[i+1][j+1] = this.getSimilarity(img, x+i, y+j, pattern);
+					sim[i+1][j+1] = getSimilarity(img, x+i, y+j, pattern);
 				}	
 			}
 
@@ -72,39 +73,44 @@ public class FindPattern {
 			}
 
 			if(sim[xTemp][yTemp] > thrRight){
-				resultLineRight[index] = true;
+				res[index] = true;
 				y += yTemp - 1;
 				x += xTemp - 1;
 			} else {
-				resultLineRight[index] = false;
+				res[index] = false;
 			}
 		}
 
 		for(; index < 100; index++){
-			resultLineRight[index] = false;
+			res[index] = false;
 		}
 
-		return resultLineRight;
+		return res;
 	}
 	
 	public float getSimilarity(BufferedImage img, int x, int y, BufferedImage pattern){
+		int cacheCounter = 0;
 		float diff = 0;
+
 		int width = pattern.getWidth();
 		int height = pattern.getHeight();
 
+		BufferedImage cache = srv.map(width, height);
+
 		int counter = 0;
+
 		float diameter = (float) Math.sqrt((width*width + height*height)) / 2;
 		for(int i = 0; i < width; i++){
 			for(int j = 0; j < height; j++){
 				counter++;
 				if((x+i) >= img.getWidth() || (y+j) >= img.getHeight() || img.getRGB(x+i, y+j) != pattern.getRGB(i, j)){
 					diff += (float) Math.sqrt((width/2 - i)*(width/2 - i) + (height/2 - j)*(height/2 - j)) / diameter;
+				} else{
 				}
 			}
 		}
 
-		float result = 1 - diff / counter;//(width * height);
-		result = result * result * result * result;
-		return result;
+		float res = 1 - diff / counter;//(width * height);
+		return res*res*res*res;
 	}
 }
